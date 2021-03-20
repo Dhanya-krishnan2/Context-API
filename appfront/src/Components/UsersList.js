018937e9db686daa0699298aeecacb216f146077
Dhanya-krishnan2 <dhanyakunhi@gmail.com>
2021-03-21 00:36:05 +0800

    added component in home

--- appfront/src/Components/UsersList.js
+++ appfront/src/Components/UsersList.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+import { 
+
+    ListGroup,
+    ListGroupItem,
+    Button,
+
+} from 'reactstrap';
 
 function UsersList() {
     return (
-        <div>
-        <h1> I am the userlist</h1>
-        </div>
+        <ListGroup>
+            <ListGroupItem className="d-flex">
+            <strong> I am the user one list </strong>
+        <div className="ml-auto">
+        <Link to='/edituser' className='btn btn-warning mr-1'> edit</Link>
+        <Button color="danger">Delete</Button>
+        </div> 
+
+        </ListGroupItem>
+
+        </ListGroup>
     )
 }
 
